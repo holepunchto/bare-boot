a864147e8e54d168d72733b472d88a00cf845fbe
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-10-13 12:48:20 +0200

    Write platform prebuilds to `Addon.path`

--- index.js
+++ index.js
@@ -1,36 +1,100 @@
+const fs = require('fs/promises')
 const path = require('path')
 const Module = require('module')
+const Addon = require('addon')
 
-module.exports = async function boot (drive) {
-  const root = drive.root || '/'
+module.exports = async function boot (drive, opts = {}) {
+  const {
+    platform = process.platform,
+    arch = process.arch,
+    cwd = process.cwd()
+  } = opts
 
-  const files = new Map()
+  const previousCwd = process.cwd()
 
-  for await (const entry of drive.list()) {
-    files.set(path.join(root, entry.key), await drive.get(entry.key))
-  }
+  if (cwd !== previousCwd) process.chdir(cwd)
 
-  let main = path.join(root, 'index.js')
+  try {
+    const root = drive.root || cwd
 
-  if (files.has(path.join(root, 'package.json'))) {
-    const pkg = JSON.parse(files.get(path.join(root, 'package.json')))
+    const files = new Map()
 
-    if (pkg.main) {
-      main = path.join(root, pkg.main)
-    }
-  }
+    for await (const entry of drive.list()) {
+      switch (path.extname(entry.key)) {
+        case '.bare':
+        case '.node':
+          await writePrebuild(drive, entry, platform, arch)
+          break
 
-  const module = Module.load(main, {
-    protocol: new Module.Protocol({
-      exists (filename) {
-        return files.has(filename)
-      },
-
-      read (filename) {
-        return files.get(filename)
+        default:
+          files.set(path.join(root, entry.key), await drive.get(entry.key))
       }
-    })
-  })
+    }
 
-  return module.exports
+    let main = path.join(root, 'index.js')
+
+    if (files.has(path.join(root, 'package.json'))) {
+      const pkg = JSON.parse(files.get(path.join(root, 'package.json')))
+
+      if (pkg.main) {
+        main = path.join(root, pkg.main)
+      }
+    }
+
+    const module = Module.load(main, {
+      protocol: new Module.Protocol({
+        exists (filename) {
+          return files.has(filename)
+        },
+
+        read (filename) {
+          return files.get(filename)
+        }
+      })
+    })
+
+    return module.exports
+  } finally {
+    if (cwd !== previousCwd) process.chdir(previousCwd)
+  }
+}
+
+async function writePrebuild (drive, entry, platform, arch) {
+  if (Addon.path === null) return
+
+  if (path.basename(path.dirname(entry.key)) !== `${platform}-${arch}`) return
+
+  let pkg = null
+  let dirname = entry.key
+  do {
+    dirname = path.dirname(dirname)
+
+    pkg = await drive.get(path.join(dirname, 'package.json'))
+
+    if (pkg) break
+  } while (dirname !== '/' && dirname !== '.')
+
+  if (pkg === null) return
+
+  let info
+  try {
+    info = JSON.parse(pkg.toString())
+  } catch {
+    info = null
+  }
+
+  if (info === null || typeof info.name !== 'string') return
+
+  let target = path.join(Addon.path, info.name)
+
+  if (info.version) target += `@${info.version}`
+
+  target += '.bare'
+
+  try {
+    await fs.access(target)
+  } catch {
+    await fs.mkdir(path.dirname(target), { recursive: true })
+    await fs.writeFile(target, await drive.get(entry.key))
+  }
 }
--- test.js
+++ test.js
@@ -1,24 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+const Addon = require('addon')
 const test = require('brittle')
+const tmp = require('test-tmp')
 const boot = require('.')
 const { localdrive, hyperdrive } = require('./test/helpers')
 
 test('file system', async (t) => {
   const drive = localdrive(t, 'test/fixtures/basic')
 
-  t.is(await boot(drive), 'Hello from drive')
+  t.is(await boot(drive, { cwd: await tmp(t) }), 'Hello from drive')
 })
 
 test('file system, addon', async (t) => {
   const drive = localdrive(t, 'test/fixtures/addon')
 
-  t.is(await boot(drive), 'Hello from addon')
+  t.is(await boot(drive, { cwd: await tmp(t) }), 'Hello from addon')
 })
 
 test('memory', async (t) => {
   const drive = await hyperdrive(t)
 
-  await drive.put('package.json', '{ "main": "entry.js" }')
-  await drive.put('entry.js', 'module.exports = \'Hello from drive\'')
+  await localdrive(t, 'test/fixtures/basic').mirror(drive).done()
 
-  t.is(await boot(drive), 'Hello from drive')
+  t.is(await boot(drive, { cwd: await tmp(t) }), 'Hello from drive')
+})
+
+test('memory, addon', async (t) => {
+  const drive = await hyperdrive(t)
+
+  await localdrive(t, 'test/fixtures/addon').mirror(drive).done()
+
+  const cwd = await tmp(t)
+
+  Addon.path = path.join(cwd, 'prebuilds')
+
+  t.is(await boot(drive, { cwd }), 'Hello from addon')
 })
